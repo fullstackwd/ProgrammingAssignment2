4282a621ec4a1bd996dee4f4155ba33d6be1e3fd
Alef <83234291+fullstackwd@users.noreply.github.com>
2024-07-17 21:50:37 -0300

    Update cachematrix.R
    make

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,69 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  # Function to set the matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # Function to get the matrix
+  get <- function() {
+    x
+  }
+  
+  # Function to set the inverse of the matrix
+  setInverse <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  # Function to get the inverse of the matrix
+  getInverse <- function() {
+    inv
+  }
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  # Check if the inverse is already cached
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # Get the matrix from the special object
+  data <- x$get()
+  
+  # Compute the inverse of the matrix
+  inv <- solve(data, ...)
+  
+  # Cache the inverse
+  x$setInverse(inv)
+  
+  inv
 }
+
+# Example
+# Create a matrix
+mat <- matrix(c(1, 2, 3, 4), 2, 2)
+
+# Create the special matrix object
+specialMatrix <- makeCacheMatrix(mat)
+
+# Calculate and cache the inverse
+inverse1 <- cacheSolve(specialMatrix)
+print(inverse1)
+
+# Retrieve the cached inverse
+inverse2 <- cacheSolve(specialMatrix)
+print(inverse2)
+
